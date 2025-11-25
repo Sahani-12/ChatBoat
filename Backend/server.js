74878342409a547477431742d045b783d53bb048
Anand Sahani <anandsahani617@gmail.com>
2025-11-25 20:44:23 +0530

    some change

--- Backend/server.js
+++ Backend/server.js
@@ -13,8 +13,8 @@ const PORT = 8080;
 // 1. Postman se JSON lene ke liye
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://chat-boat-six.vercel.app/"], // यहाँ अपना Vercel लिंक डालें
-    credentials: true
+  origin: "*", 
+  credentials: true
 }));
 
 app.use("/api", chatRoutes);
